90b6b7fc78038569f8750143e41821dc25044799
Ankita Sharma <asharma@tavisca.com>
2018-09-18 15:53:28 +0530

    Hotel search application fetching data using json file

---
+++ SearchHotelWCF/SearchHotelWCF/IHotelSearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchHotelWCF
+{
+    [ServiceContract]
+    public interface IHotelSearch
+    {
+        [OperationContract]
+        List<HotelModel> GetAllHotels();
+
+        [OperationContract]
+        HotelModel GetHotelById(int Id);
+    }
+}
